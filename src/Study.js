35f8d0b702fedca47104c3ade1ad1c6da3999911
4thdraw <4thdraw2@gmail.com>
2025-04-01 10:15:08 +0900

    useState이용해서 처리

--- src/Study.js
+++ src/Study.js
@@ -1,11 +1,22 @@
-import React from 'react';
-
+import { useRef, useState } from 'react';
 const Study = () => {
+    const buttonRef = useRef();
+    const textarry =["열기", "닫기"];    
+
+    const [btn, setBtn] = useState(0);    
+
+    const handleClick = () => {
+        setBtn((prevBtn) => (prevBtn + 1) % textarry.length);     
+    };
+
     return (
         <div>
-            
+            <button ref={buttonRef} onClick={handleClick}>
+                {textarry[btn]}
+            </button>
         </div>
     );
 };
+
 
 export default Study;
